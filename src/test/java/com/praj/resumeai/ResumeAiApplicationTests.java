58c1deffd4c6c3bab51f05fa82e45025b4d4b51c
Prajwalkadam29 <prajwal.kadam2005@gmail.com>
2025-12-30 15:49:47 +0530

    Initial commit

---
+++ src/test/java/com/praj/resumeai/ResumeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praj.resumeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
